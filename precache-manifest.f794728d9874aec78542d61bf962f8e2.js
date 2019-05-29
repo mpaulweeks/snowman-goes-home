fe837f57e9cb5aa0f83b29b235da199e5d0c27c9
mpaulweeks <mpaulweeks@gmail.com>
2019-05-29 00:22:48 -0400

    Updates

---
+++ precache-manifest.f794728d9874aec78542d61bf962f8e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86095a7a69dc6389241992f4bd7de5d8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d8edf04146664d177caf",
+    "url": "./static/css/main.685dea8f.chunk.css"
+  },
+  {
+    "revision": "95ca583606faf0b3824c",
+    "url": "./static/js/2.e5316ad4.chunk.js"
+  },
+  {
+    "revision": "d8edf04146664d177caf",
+    "url": "./static/js/main.7d84ce32.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
