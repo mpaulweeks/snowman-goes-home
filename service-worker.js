05287d2da093b4689d940557dac40cc823959ad5
mpaulweeks <mpaulweeks@gmail.com>
2019-06-09 18:03:22 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.d5f276a0956a79cf9d892d40177d707e.js"
+  "./precache-manifest.660768de0d3e68930a63f99472a7addc.js"
 );
 
 self.addEventListener('message', (event) => {
